db598c42c3b8aae34e0a906fc4751ab83b02f734
Syafiq Afifuddin <syafiqafifuddin59@gmail.com>
2021-03-08 21:10:58 +0700

    new link, active class each page

--- resources/views/admin/pengaduan_index.blade.php
+++ resources/views/admin/pengaduan_index.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('pengaduan-link', 'active')
 
 @section('content')
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,15 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                        @role('admin')
+                        <li class="nav-item">
+                            <a class="nav-link @yield('pengaduan-link')" href="{{ route('admin.index') }}">{{ __('Pengaduan') }}</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link @yield('userman-link')" href="{{ route('userman.index') }}">{{ __('User Management') }}</a>
+                        </li>
+                        @endrole
 
                     </ul>
 
@@ -54,12 +63,6 @@
                         </li>
                         @endif
                         @else
-
-                        @role('admin')
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('userman.index') }}">{{ __('User Management') }}</a>
-                        </li>
-                        @endrole
 
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
