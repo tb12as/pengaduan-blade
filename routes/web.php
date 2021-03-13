87adf09e811e503c2f4a07de781d0eae6fff0b7f
Syafiq Afifuddin <syafiqafifuddin59@gmail.com>
2021-03-13 11:18:46 +0700

    remove middleware on __construct HomeController

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ use App\Http\Controllers\Masyarakat\PengaduanController;
 use App\Http\Controllers\Admin\PengaduanController as PengaduanAController;
 use App\Http\Controllers\Admin\TanggapanController;
 use App\Http\Controllers\Admin\UserManagementController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MasyarkatController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,9 +16,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
     Route::middleware('role:masyarakat')->prefix('masyarakat')->group(function () {
         Route::get('', [MasyarkatController::class, 'index'])->name('masyarakat.index');
         Route::resource('pengaduan', PengaduanController::class)->parameters([
@@ -36,7 +38,7 @@ Route::middleware('auth')->group(function () {
         Route::middleware(['role:admin'])->group(function () {
             Route::get('/cetak/{pengaduan:slug}', [CetakController::class, 'cetak'])->name('cetak');
 
-            Route::prefix('user-management')->group(function() {
+            Route::prefix('user-management')->group(function () {
                 Route::get('/', [UserManagementController::class, 'index'])->name('userman.index');
                 Route::get('/petugas/create', [UserManagementController::class, 'create_petugas'])->name('petugas.create');
                 Route::post('/petugas/', [UserManagementController::class, 'petugas_store'])->name('petugas.store');
@@ -45,7 +47,6 @@ Route::middleware('auth')->group(function () {
 
                 Route::patch('/petugas/{user:id}/update', [UserManagementController::class, 'petugas_update'])->name('petugas.update');
             });
-
         });
     });
 });
